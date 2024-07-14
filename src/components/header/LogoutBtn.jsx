de18417341084a1408c7268a6251f146a1ba5f3b
Anand Raj <85210549+Anandraj2908@users.noreply.github.com>
2024-07-14 23:05:24 +0530

    Update LogoutBtn.jsx

--- src/components/header/LogoutBtn.jsx
+++ src/components/header/LogoutBtn.jsx
@@ -5,14 +5,17 @@ import { logout } from '../../store/authSlice'
 import { useDispatch } from 'react-redux'
 
 import Button from '../button/Button'
+import { useNavigate } from 'react-router-dom'
 
 const LogoutBtn = () => {
     const dispatch =  useDispatch();
+    const navigate = useNavigate();
 
     const logoutHandeler = () => {
         authService.logout().then(()=>{
             dispatch(logout())
         })
+        navigate('/')
     }
   return (
     <div onClick={logoutHandeler} >
